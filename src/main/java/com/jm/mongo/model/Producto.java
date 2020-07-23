67e70c230ca3eb36437221ba944f2f9b4dde32ae
Joel Medina <joelsalvadormedinaosornio@gmail.com>
2020-07-22 21:23:09 -0500

    1.0.1.0 - Se eliminan getters y setters arcaicos

--- src/main/java/com/jm/mongo/model/Producto.java
+++ src/main/java/com/jm/mongo/model/Producto.java
@@ -3,6 +3,7 @@ package com.jm.mongo.model;
 import java.util.Date;
 import java.util.UUID;
 
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,6 +12,7 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+@Data
 @Document(collection = "Products")
 public class Producto {
 	
@@ -29,53 +31,5 @@ public class Producto {
 	
 	public Producto() {
 		
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public Double getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(Double precio) {
-		this.precio = precio;
-	}
-
-	public Date getCaducidad() {
-		return caducidad;
-	}
-
-	public void setCaducidad(Date caducidad) {
-		this.caducidad = caducidad;
-	}
-
-	public Integer getCantidad() {
-		return cantidad;
-	}
-
-	public void setCantidad(Integer cantidad) {
-		this.cantidad = cantidad;
 	}
 }
